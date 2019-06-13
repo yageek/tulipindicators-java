7235949e6616b041f9fd59edff427c21e8be79ad
Yannick Heinrich <yannick.heinrich@gmail.com>
2019-06-13 09:59:50 +0200

    Wrapping JNI with include options

--- tulip/include/tulip-wrapping.h
+++ tulip/include/tulip-wrapping.h
@@ -4,8 +4,6 @@
 #ifndef __TULIP_JNI_WRAPPING
 #define __TULIP_JNI_WRAPPING
 
-extern "C"
-{
-    JNIEXPORT void JNICALL Java_simplejni_Main_printHello(JNIEnv *, jobject);
-}
+JNIEXPORT void JNICALL Java_simplejni_Main_printHello(JNIEnv *, jobject);
+
 #endif
